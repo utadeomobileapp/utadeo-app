10ff2fade0cb280702ac98ec5c647c2fb4380bfc
Felipe Sebastian Camilo Silva Florez <f.silva@globant.com>
2019-06-11 22:22:59 -0500

    [Architecture][Felipe Silva] - First commit adding baseline

---
+++ screens/LinksScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { ExpoLinksView } from '@expo/samples';
+
+export default function LinksScreen() {
+  return (
+    <ScrollView style={styles.container}>
+      {
+        //// TODO: import Links
+      }
+      <ExpoLinksView />
+    </ScrollView>
+  );
+}
+
+LinksScreen.navigationOptions = {
+  title: 'Links',
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
